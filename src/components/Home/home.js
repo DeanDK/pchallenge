a4a5d62874952ed6a334c844f4d76a7886806f5a
DeanDK <deanbozic12@gmail.com>
2018-12-13 20:02:45 +0100

    initial setup with redux, routes and json server

---
+++ src/components/Home/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import HomeContainer from "./../../containers/home_container.js";
+const Home = () => (
+  <div>
+    <HomeContainer />
+  </div>
+);
+export default Home;
