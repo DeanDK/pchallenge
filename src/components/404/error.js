3d7f2a3d5c5da5bd15e9ae95d20eb263c2e00b17
DeanDK <deanbozic12@gmail.com>
2018-12-13 23:03:43 +0100

    added no route error

---
+++ src/components/404/error.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const NoRoute = ({ location }) => <div>No match for {location.pathname}</div>;
+
+export default NoRoute;
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home/home";
+import NoRoute from "./components/404/error.js";
 import Layout from "./hoc/layout.js";
 
 const Routes = () => (
-  <Switch>
-    <Layout>
-      <Route path="/" exact component={Home} />
-    </Layout>
-  </Switch>
+  <Layout>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route component={NoRoute} />
+    </Switch>
+  </Layout>
 );
 
 export default Routes;
