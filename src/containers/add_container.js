7adeeadad733f234155980fffe0d5fde6b2ed8e9
DeanDK <deanbozic12@gmail.com>
2018-12-15 02:50:12 +0100

    added on change event listeners to add wine

--- src/containers/add_container.js
+++ src/containers/add_container.js
@@ -1,19 +1,51 @@
 import React, { Component } from "react";
 
 class AddContainer extends Component {
+  state = {
+    wine: {
+      title: "",
+      country: ",",
+      year: ""
+    }
+  };
+
+  onTextChange = e => {
+    let { wine } = this.state;
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    wine[fieldName] = fieldValue;
+    this.setState({ wine });
+  };
+
   render() {
     return (
       <div className="add_form_body">
         <div className="add_form_body__content">
           <div className="add_form_body__content-title">Add Wine</div>
           <div className="add_form_body__content-input-title">
-            <input type="text" placeholder="title" />
+            <input
+              type="text"
+              placeholder="title"
+              name="title"
+              onChange={this.onTextChange}
+            />
           </div>
           <div className="add_form_body__content-input-country">
-            <input type="text" placeholder="country" />
+            <input
+              type="text"
+              placeholder="country"
+              name="country"
+              onChange={this.onTextChange}
+            />
           </div>
           <div className="add_form_body__content-input-year">
-            <input type="text" placeholder="year" />
+            <input
+              type="text"
+              placeholder="year"
+              name="year"
+              onChange={this.onTextChange}
+            />
           </div>
           <div className="add_form_body__content-input-submit">
             <input type="submit" />
