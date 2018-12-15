331bc1ad53032eb3868d6c6890c5f5a5c05b1622
DeanDK <deanbozic12@gmail.com>
2018-12-15 03:16:06 +0100

    added logic for verification of form fields

--- src/containers/add_container.js
+++ src/containers/add_container.js
@@ -18,7 +18,21 @@ class AddContainer extends Component {
     this.setState({ wine });
   };
 
+  formVerification = e => {
+    const array = Object.values(this.state.wine);
+    return array.every(this.notEmpty);
+  };
+
+  notEmpty = element => {
+    return element !== "";
+  };
+
+  submit = () => {
+    // ... formVerification
+  };
+
   render() {
+    // form + onSumbit better option
     return (
       <div className="add_form_body">
         <div className="add_form_body__content">
@@ -48,7 +62,7 @@ class AddContainer extends Component {
             />
           </div>
           <div className="add_form_body__content-input-submit">
-            <input type="submit" />
+            <input type="submit" onClick={this.submit} />
           </div>
         </div>
       </div>
